82624d5ee0e1a8dbcd052455fe41e4d22ea4a099
Abdul <46938888+AbduLCod3@users.noreply.github.com>
2025-09-23 18:34:54 -0500

    NavBar changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
-//import "./styles/layout.scss";
 import "./styles/layoutv2.scss";
-import Image from "next/image";
-import logo from "../../public/images/dallaalkii_logo_v6.1.png";
-import menu from "../../public/images/menu.png";
+import Navbar from "../components/Navbar";
+
 export const metadata = { title: "Dallaalkii.com" };
 
 export default function RootLayout({
@@ -11,45 +9,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="layout">
-      <html lang="en">
-        <body>
-          <header>
-            <nav>
-              <div className="left">
-                <a href="/" className="logo">
-                  <Image src={logo} alt="Dallaalkii Logo" />
-                </a>
-                <a href="/">Home</a>
-                <a href="/buy">Buy</a>
-                <a href="/rent">Rent</a>
-                <a href="/agents">Agents</a>
-                <a href="/developers">Developers</a>
-              </div>
-              <div className="right">
-                <a href="/contact">Contact Us</a>
-                <a href="/Login" className="login">
-                  Login
-                </a>
-                <div className="menuIcon">
-                  <Image src={menu} alt="Menu Icon" />
-                </div>
-                <div className="menu">
-                  <a href="/">Home</a>
-                  <a href="/buy">Buy</a>
-                  <a href="/rent">Rent</a>
-                  <a href="/agents">Agents</a>
-                  <a href="/developers">Developers</a>
-                  <a href="/contact">Contact Us</a>
-                  <a href="/Login">Login</a>
-                </div>
-              </div>
-            </nav>
-          </header>
+    <html lang="en">
+      <body>
+        <div className="layout">
+          <Navbar />
           <main>{children}</main>
           <footer>© {new Date().getFullYear()} Dallaalkii</footer>
-        </body>
-      </html>
-    </div>
+        </div>
+      </body>
+    </html>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,45 @@
+"use client";
+import Image from "next/image";
+import logo from "../../public/images/dallaalkii_logo_v6.1.png";
+import menu from "../../public/images/menu.png";
+import { useState } from "react";
+
+export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <header>
+      <nav>
+        <a href="/" className="logo">
+          <Image src={logo} alt="Dallaalkii Logo" />
+        </a>
+        <div className="left">
+          <a href="/">Home</a>
+          <a href="/buy">Buy</a>
+          <a href="/rent">Rent</a>
+          <a href="/agents">Agents</a>
+          <a href="/developers">Developers</a>
+        </div>
+        <div className="right">
+          <a href="/contact">Contact Us</a>
+          <a href="/Login" className="login">
+            Login
+          </a>
+        </div>
+        <div className="menuIcon">
+          <Image src={menu} alt="Menu Icon" onClick={() => setOpen(!open)} />
+        </div>
+
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/buy">Buy</a>
+          <a href="/rent">Rent</a>
+          <a href="/agents">Agents</a>
+          <a href="/developers">Developers</a>
+          <a href="/contact">Contact Us</a>
+          <a href="/Login">Login</a>
+        </div>
+      </nav>
+    </header>
+  );
+}
